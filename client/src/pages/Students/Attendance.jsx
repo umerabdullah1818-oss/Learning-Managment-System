a43d4f4f57e2dd114a2243bf354ca47089f1647d
bkrgill <abubakargill326@gmail.com>
2026-05-18 02:48:01 +0500

    Fix casing import issue for StudentAttendance

--- client/src/pages/Students/Attendance.jsx
+++ client/src/pages/Students/Attendance.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Layout from '../layouts/Layout';
-import StudentAttendance from '../../components/students/Attendance/StudentAttendance';
+import StudentAttendance from '../../components/students/attendance/StudentAttendance';
 import { toast } from 'react-toastify';
 import { API_BASE_URL } from '../../config/api';
 
